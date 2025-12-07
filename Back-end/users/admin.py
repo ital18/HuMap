28d05904fba78c517a95a9990eda25affbed59ae
Milenna Xavier <milennasx@gmail.com>
2025-12-07 01:59:36 -0300

    Integração total do Login (Front+Back), Configuração de CORS e Custom User Model

--- Back-end/users/admin.py
+++ Back-end/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+# Isto diz ao Django para mostrar o seu utilizador no painel /admin
+admin.site.register(User, UserAdmin)
